5c4b9bfe27c56be28fa140f0bf55b8b875ad9e58
AA9-YG <56201552+AA9-YG@users.noreply.github.com>
2022-06-02 12:18:48 -0500

    Update yt_video.py

--- resources/lib/youtube_plugin/youtube/helper/yt_video.py
+++ resources/lib/youtube_plugin/youtube/helper/yt_video.py
@@ -107,7 +107,7 @@ def _process_more_for_video(provider, context):
                    'Container.Update(%s)' % context.create_uri(['special', 'parent_comments'], {'video_id': video_id})),
                   (context.localize(provider.LOCAL_MAP['youtube.video.description.links']),
                    'Container.Update(%s)' % context.create_uri(['special', 'description_links'], {'video_id': video_id})),
-                  (context.localize(provider.LOCAL_MAP['youtube.video.descriptions'], str(context.get_param(['youtube.video.more']))),
+                  (context.localize(provider.LOCAL_MAP['youtube.video.descriptions'], str(context.get_param('youtube.video.more'))),
                    'Container.Update(%s)' % context.create_uri(['special', 'related_videos'], {'video_id': video_id}))])
 
     if is_logged_in == '1':
