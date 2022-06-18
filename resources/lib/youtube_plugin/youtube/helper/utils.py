06327e2959ccdc16b57065cfe870e288b4ee26e0
AA9-YG <56201552+AA9-YG@users.noreply.github.com>
2022-06-17 21:49:08 -0500

    Update utils.py

--- resources/lib/youtube_plugin/youtube/helper/utils.py
+++ resources/lib/youtube_plugin/youtube/helper/utils.py
@@ -344,6 +344,7 @@ def update_video_infos(provider, context, video_id_dict, playlist_item_id_dict=N
             video_item.set_aired_utc(utils.datetime_parser.strptime(snippet['publishedAt']))
 
         if datetime:
+            datetime.day = datetime.day + 1
             video_item.set_year_from_datetime(datetime)
             video_item.set_aired_from_datetime(datetime)
             video_item.set_premiered_from_datetime(datetime)
