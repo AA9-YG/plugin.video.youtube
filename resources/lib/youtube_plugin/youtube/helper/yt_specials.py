2c0e5388d60f8ba4253aaccef5e3d9519af3d2d9
AA9-YG <56201552+AA9-YG@users.noreply.github.com>
2022-06-17 19:11:55 -0500

    Update yt_specials.py

--- resources/lib/youtube_plugin/youtube/helper/yt_specials.py
+++ resources/lib/youtube_plugin/youtube/helper/yt_specials.py
@@ -156,8 +156,8 @@ def _process_video_stats(provider, context):
     snippet = yt_item['snippet']  # crash if not conform
     
     datetime = snippet['publishedAt']
-    dur = yt_item.get('contentDetails', {}).get('duration', '')
-    length = kodion.utils.datetime_parser.parse(dur)
+    #dur = yt_item.get('contentDetails', {}).get('duration', '')
+    #length = kodion.utils.datetime_parser.parse(dur)
     
     #stats = []
     #stats.extend(response.text)
@@ -169,7 +169,7 @@ def _process_video_stats(provider, context):
     likes = '[B]Likes: [COLOR lime]%s[/COLOR][/B]\n' % stats['likes']
     dislikes = '[B]Dislikes: [COLOR red]%s[/COLOR][/B]\n' % stats['dislikes']
     date = '[B]Published Date: %s[/B]\n' % datetime
-    duration = '[B]Duration: %s[/B]\n' % length
+    duration = '[B]Duration: [/B]\n' 
     vid_id = '[B]Video ID: %s\n[/B]' % video_id
     description = '[B]\nDescription:[/B] %s' % kodion.utils.strip_html_from_text(snippet['description'])
     
