e379c7ccd1ea425e2eb9a67a372c0f20f4b66ac8
AA9-YG <56201552+AA9-YG@users.noreply.github.com>
2022-07-02 00:47:31 -0500

    Update yt_video.py

--- resources/lib/youtube_plugin/youtube/helper/yt_video.py
+++ resources/lib/youtube_plugin/youtube/helper/yt_video.py
@@ -103,7 +103,9 @@ def _process_more_for_video(provider, context):
     items.extend([(context.localize(provider.LOCAL_MAP['youtube.video.play_ask_for_quality']),
                   'RunPlugin(%s)' % context.create_uri(['play'], {'video_id': video_id, 'ask_for_quality': '1'})),
                   (context.localize(provider.LOCAL_MAP['youtube.video.play_with_subtitles']),
-                  'RunPlugin(%s)' % context.create_uri(['play'], {'video_id': video_id, 'prompt_for_subtitles': '1'}))])
+                  'RunPlugin(%s)' % context.create_uri(['play'], {'video_id': video_id, 'prompt_for_subtitles': '1'}))
+                  (context_menu.append((context.localize(provider.LOCAL_MAP['youtube.video.play_audio_only']),
+                  'RunPlugin(%s)' % context.create_uri(['play'], {'video_id': video_id, 'audio_only': '1'}))])
 
     # default items
     #items.extend([#(context.localize(provider.LOCAL_MAP['youtube.related_videos']),
