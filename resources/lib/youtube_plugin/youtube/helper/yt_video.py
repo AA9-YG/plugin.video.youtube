4cd360f56286a6f2815ad3e08c4806192ec67605
AA9-YG <56201552+AA9-YG@users.noreply.github.com>
2022-07-02 00:42:56 -0500

    Update yt_video.py

--- resources/lib/youtube_plugin/youtube/helper/yt_video.py
+++ resources/lib/youtube_plugin/youtube/helper/yt_video.py
@@ -99,9 +99,11 @@ def _process_more_for_video(provider, context):
         items.append((context.localize(provider.LOCAL_MAP['youtube.video.add_to_playlist']),
                       'RunPlugin(%s)' % context.create_uri(['playlist', 'select', 'playlist'], {'video_id': video_id})))
     
-    # play (ask for quality)
-    items.append((context.localize(provider.LOCAL_MAP['youtube.video.play_ask_for_quality']),
-                         'RunPlugin(%s)' % context.create_uri(['play'], {'video_id': video_id, 'ask_for_quality': '1'})))
+    # more play options 
+    items.extend([(context.localize(provider.LOCAL_MAP['youtube.video.play_ask_for_quality']),
+                  'RunPlugin(%s)' % context.create_uri(['play'], {'video_id': video_id, 'ask_for_quality': '1'})),
+                  (context.localize(provider.LOCAL_MAP['youtube.video.play_with_subtitles']),
+                  'RunPlugin(%s)' % context.create_uri(['play'], {'video_id': video_id, 'prompt_for_subtitles': '1'}))])
 
     # default items
     #items.extend([#(context.localize(provider.LOCAL_MAP['youtube.related_videos']),
