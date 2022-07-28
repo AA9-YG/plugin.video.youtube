756cef018688d9fc20904861121310a286ba8b13
AA9-YG <56201552+AA9-YG@users.noreply.github.com>
2022-07-28 16:22:03 -0500

    Update play_info.py

--- resources/lib/youtube_plugin/youtube/helper/play_info.py
+++ resources/lib/youtube_plugin/youtube/helper/play_info.py
@@ -26,3 +26,57 @@ def num_fmt(num):
             return '{:{fmt}}{sym}'.format(num/10.**e_thresh, fmt=fmt, sym=sym)
     
     return '{:{fmt}}'.format(num, fmt=fmt)
+
+
+def get_play_info(provider, context):
+    video_id = context.get_param('video_id', '')
+    channel_id = context.get_param('channel_id', '')
+    channel_name = context.get_param('channel_name', '')
+    key = "AIzaSyAT-LCjBFiQdRdYTO0XL312EIFx6SKm1lU"
+    vid_url = 'https://returnyoutubedislikeapi.com/votes?videoId=' + str(video_id)
+    vid_url2 = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + channel_id + "&key=" + key
+    
+    response = requests.get(vid_url)
+    response2 = requests.get(vid_url2)
+    
+    provider.set_content_type(context, kodion.constants.content_type.VIDEOS)
+    resource_manager = provider.get_resource_manager(context)
+    
+    video_data = resource_manager.get_videos([video_id])
+    yt_item = video_data[video_id]
+    snippet = yt_item['snippet']  # crash if not conform
+    
+    dt = snippet['publishedAt']
+    datetime = kodion.utils.datetime_parser.strptime(dt, fmt='%Y-%m-%dT%H:%M:%S.%fZ')
+        
+    dt_string = '%s/%s/%s' % (datetime.month, datetime.day, datetime.year)
+
+    stats = response.json()
+    channel_stats = response2.json()
+    
+    view_count = "{:,}".format(stats['viewCount'])
+    like_count = "{:,}".format(stats['likes'])
+    dislike_count = "{:,}".format(stats['dislikes'])
+    c_stats = channel_stats['items'][0]['statistics']['subscriberCount']
+    
+    try:
+        c_stats2 = int(c_stats)
+        context.log_debug('Sub count after int conversion: %s' % c_stats)
+    except (TypeError, ValueError):
+        sub_count = 'No Subscribers or Hidden'
+        context.log_debug('This sub count is in the exception block')
+    else:
+        if c_stats2 > 0:
+            sub_count = "{:,}".format(c_stats2)
+            context.log_debug('Sub count is greater than zero')
+        else:
+            sub_count = 'No Subscribers or Hidden'
+            context.log_debug('Sub count is not greater than zero')
+        
+    subscribers = '[B]Subscribers: %s[/B]\n' % sub_count
+    views = '[B]Views: [COLOR cyan]%s[/COLOR][/B]\n' % view_count 
+    likes = '[B]Likes: [COLOR lime]%s[/COLOR][/B]\n' % like_count
+    dislikes = '[B]Dislikes: [COLOR red]%s[/COLOR][/B]\n' % dislike_count
+    date = '[B]Published Date: %s[/B]\n' % dt_string
+    
+    vid_info = subscribers + views + likes + dislikes + date
